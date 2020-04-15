23bfbd600e80673aa4df1e84cc62c526016ae61f
WONGHiuFung <62242996+WONGHiuFung@users.noreply.github.com>
2020-04-15 21:46:47 +0800

    changed menu schema into some sort of array
    1. changed menu schema into some sort of array, for easier frontend retrieveing data

--- cufoodorder_backend_server(use_this)/models/menu.js
+++ cufoodorder_backend_server(use_this)/models/menu.js
@@ -4,10 +4,10 @@ var Schema = mongoose.Schema;
 var MenuSchema = new Schema({
     restaurantName: {type: Schema.Types.ObjectId, ref:'Customer'},
     menuName: {type: String},
-    menuList: {type: String},
-    description: {type: String},
-    price: {type: Number, default:0},
-    imageAddress: {type: String},
+    menuList: [{dish: String, description: String, price: {type: Number, default: 0}, imageAddress: String}],
+    // description: {type: String},
+    // price: {type: Number, default:0},
+    // imageAddress: {type: String},
     likes: {type: Number}
 });
 

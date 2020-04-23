028dc27d1204b08e40840e083fd00d1d531305b2
WONGHiuFung <62242996+WONGHiuFung@users.noreply.github.com>
2020-04-23 19:29:31 +0800

    Update chat.js
    bugs fixed for chatapp

--- cufoodorder_backend_server(use_this)/middlewares/chat.js
+++ cufoodorder_backend_server(use_this)/middlewares/chat.js
@@ -7,7 +7,7 @@ var customers = [];
 //let cs can freely join any customer room
 var addCustomer = ({ connection_id, customer_room }) => {
 
-  var customer = { connection_id, username: customer_room, customer_room, cs_flag: "no"};
+  var customer = { connection_id: connection_id, username: customer_room, customer_room: customer_room, cs_flag: "no"};
 
   customers.push(customer);
 
@@ -33,13 +33,22 @@ var infoCustomer = (connection_id) => customers.find((customer) => customer.conn
 //   }
 // };
 
-var addcs = ({cs_name, customer_room}) => {
-  var customer = { connection_id, username: cs_name, customer_room, cs_flag: "yes"};
+var addcs = ({connection_id, cs_name, customer_room}) => {
+  var customer = { connection_id: connection_id, username: cs_name, customer_room: customer_room, cs_flag: "yes"};
 
   customers.push(customer);
 
   return customer;
 };
+
+var updatecs = ({cs_name, customer_room}) => {
+  var index = customers.findIndex((customer) => customer.customer_room === customer_room);
+  if(index !== -1) {
+    customers[index].cs_name = cs_name;
+    return customers[index];
+  }
+};
+
 
 var findemptyroom = customers.filter((item, index, array)=>{return (item.cs_flag==="no")})
 
